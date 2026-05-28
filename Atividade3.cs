db15ad94eef92f8d47ddbacf17c46b8a8155c6ea
Kenneth <lordlqft0@gmail.com>
2026-05-28 08:40:15 -0300

    Feat: Código da Atividade 3
    Aplicação de for aninhado para obter dados, utilização de variáveis para fazer a soma da matriz e printar no console.

---
+++ Atividade3.cs
@@ -0,0 +1,29 @@
+/* Crie uma matriz 2x2 com números inteiros e calcule a soma de todos os valores. */
+
+
+
+/*
+int[,] numeros = new int[2, 2];
+int soma = 0;
+
+for (int linha = 0; linha < 2; linha++)
+{
+    for (int coluna = 0; coluna < 2; coluna++)
+    {
+        Console.Write("Digite os número: ");
+        numeros[linha, coluna] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+Console.WriteLine();
+
+for (int linha = 0; linha < 2; linha++)
+{
+    for (int coluna = 0; coluna < 2; coluna++)
+    {
+        soma += numeros[linha, coluna];
+    }
+}
+
+Console.WriteLine($"A soma da matriz é: {soma}");
+*/
