9b29f70ed2d70382c697f1a4340fb40929c9e383
Kenneth <lordlqft0@gmail.com>
2026-05-28 08:45:27 -0300

    Feat: Código da Atividade 6
    Utilização de if dentro do for aninhado para fazer trocas entre números maiores e menores, para após printar no console.

---
+++ Atividade6.cs
@@ -0,0 +1,23 @@
+/* Crie uma matriz 2x3 com números inteiros e mostre qual é o maior valor da matriz. */
+
+
+
+/*
+int[,] matriz = {
+    { 4, 2, 1 },
+    { 5, 8, 7 }
+};
+int maior = matriz[0, 0];
+
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        if (matriz[i, j] > maior)
+        {
+            maior = matriz[i, j];
+        }
+    }
+}
+Console.WriteLine($"O maior valor da matriz é: {maior}");
+*/
