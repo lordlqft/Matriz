e278f8e7d45ff2d1f2f5293f97c328ca2e87319a
Kenneth <lordlqft0@gmail.com>
2026-05-28 08:37:20 -0300

    Feat: Código da Atividade 1
    Aplicação de for aninhado e incrementação da matriz

---
+++ Atividade1.cs
@@ -0,0 +1,27 @@
+/* Crie uma matriz 2x2 de nomes (digitado pelo usuário) e exiba todos os nomes usando for aninhado. */
+
+
+
+/*
+string[,] nomes = new string[2, 2];
+
+for (int linha = 0; linha < 2; linha++)
+{
+    for (int coluna = 0; coluna < 2; coluna++)
+    {
+        Console.Write("Digite o nome: ");
+        nomes[linha, coluna] = Console.ReadLine()!;
+    }
+}
+
+Console.WriteLine();
+
+for (int linha = 0; linha < 2; linha++)
+{
+    for (int coluna = 0; coluna < 2; coluna++)
+    {
+        Console.Write(nomes[linha, coluna] + "\t");
+    }
+    Console.WriteLine();
+}
+*/
