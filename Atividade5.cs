0a9773104de62c10807b26732b4ec8a19bfdcdab
Kenneth <lordlqft0@gmail.com>
2026-05-28 08:44:41 -0300

    Feat: Código da Atividade 5
    Aplicação de for aninhado para percorrer a matriz e somar cada índice, para após printar no console.

---
+++ Atividade5.cs
@@ -0,0 +1,24 @@
+/* Crie uma matriz em que cada linha representa um aluno e cada coluna representa uma nota. Calcule
+a média de cada aluno. */
+
+
+
+/*
+string[,] alunos = { { "João" }, { "Maria" }, { "Pedro" } };
+double[,] notas = {
+    {10, 6},
+    {8, 7.5},
+    {4, 10}
+};
+
+for (int linha = 0; linha < 3; linha++)
+{
+    double soma = 0;
+    for (int coluna = 0; coluna < 2; coluna++)
+    {
+        soma += notas[linha, coluna];
+    }
+    double media = soma / 2;
+    Console.WriteLine($"A média de {alunos[linha, 0]} é: {media:F2}");
+}
+*/
