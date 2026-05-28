34eb26500ac8ad49cb58bc82231ec34573f50c8f
Kenneth <lordlqft0@gmail.com>
2026-05-28 08:43:52 -0300

    Feat: Código da Atividade 4
    Utilização de variável e for aninhado para cálculo da média

---
+++ Atividade4.cs
@@ -0,0 +1,28 @@
+/* Crie uma matriz 3x2 de notas e calcule a média geral de todas as notas. Encontre uma forma que o
+divisor (do cálculo da média) seja uma variável incrementada (não atribuir um valor fixo) */
+
+
+
+/*
+double[,] notas = {
+    {10, 6},
+    {8, 7.5},
+    {4, 10}
+};
+
+double soma = 0;
+int quantidade = 0;
+
+for (int linha = 0; linha < 3; linha++)
+{
+    for (int coluna = 0; coluna < 2; coluna++)
+    {
+        soma += notas[linha, coluna];
+        quantidade++;
+    }
+}
+
+double media = soma / quantidade;
+
+Console.WriteLine($"A média geral das notas é: {media:F2}");
+*/
