f09cc5294012948ad992f4420716ddb136187bac
Kenneth <lordlqft0@gmail.com>
2026-05-28 08:39:08 -0300

    Feat: Código da Atividade 2
    Aplicação de for aninhado para obter dados e Printar valores da matriz no console

---
+++ Atividade2.cs
@@ -0,0 +1,28 @@
+/* Crie uma matriz 2x3 com números inteiros (digitado pelo usuário) e exiba todos em formato de
+tabela. */
+
+
+
+/*
+int[,] numeros = new int[2, 2];
+
+for (int linha = 0; linha < 2; linha++)
+{
+    for (int coluna = 0; coluna < 2; coluna++)
+    {
+        Console.Write("Digite o primeiro número: ");
+        numeros[linha, coluna] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+Console.WriteLine();
+
+for (int linha = 0; linha < 2; linha++)
+{
+    for (int coluna = 0; coluna < 2; coluna++)
+    {
+        Console.Write(numeros[linha, coluna] + "\t");
+    }
+    Console.WriteLine();
+}
+*/
