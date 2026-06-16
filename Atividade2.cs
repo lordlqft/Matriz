e7f520212dd8d3e58c17f19476d2d4a2e6a30f9c
Kenneth <lordlqft0@gmail.com>
2026-06-16 08:24:11 -0300

    Fix: For percorre 3 vezes ao inves de 2

--- Atividade2.cs
+++ Atividade2.cs
@@ -4,11 +4,11 @@ tabela. */
 
 
 /*
-int[,] numeros = new int[2, 2];
+int[,] numeros = new int[2, 3];
 
 for (int linha = 0; linha < 2; linha++)
 {
-    for (int coluna = 0; coluna < 2; coluna++)
+    for (int coluna = 0; coluna < 3; coluna++)
     {
         Console.Write("Digite o primeiro número: ");
         numeros[linha, coluna] = int.Parse(Console.ReadLine()!);
@@ -19,7 +19,7 @@ Console.WriteLine();
 
 for (int linha = 0; linha < 2; linha++)
 {
-    for (int coluna = 0; coluna < 2; coluna++)
+    for (int coluna = 0; coluna < 3; coluna++)
     {
         Console.Write(numeros[linha, coluna] + "\t");
     }
